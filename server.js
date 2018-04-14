767a3a005098f87ab13cb035385d5b231c6cbf23
Paul Sokolov Jr <sokolov.p64@gmail.com>
2018-04-14 13:35:49 +0300

    added parse ability to server
    Signed-off-by: Paul Sokolov Jr <sokolov.p64@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const Papa = require('papaparse');
-
 const express = require('express'),
       app = express(),
       server = require('http').Server(app),
       io = require('socket.io')(server);
 
+const table_names = [],
+      parser = require('/lib/parser.js')(require('fs'), require('papaparse'), table_names);
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
+
+server.listen(process.env.PORT || 8080);
