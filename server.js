c1239aadebbe0d9d02ead763a091a31553ed2ff0
Paul Sokolov Jr <sokolov.p64@gmail.com>
2018-05-09 22:40:03 +0300

    added new file specially for events
    Signed-off-by: Paul Sokolov Jr <sokolov.p64@gmail.com>

--- server.js
+++ server.js
@@ -12,3 +12,16 @@ app.get('/', (req, res) => {
 app.use('/client', express.static(__dirname + '/client'));
 
 server.listen(process.env.PORT || 8080);
+
+var eventHandler;
+io.sockets.on('connection', (socket) => {
+	if (!parser.complete())
+		return;
+	if (!eventHandler)
+		eventHandler = require('./lib/event')({
+			cardPool: require('./lib/game/card')(parser.get('cardData')),
+			Player: require('./lib/game/player'),
+			Game: require('./lib/game/game')
+		});
+	eventHandler.work(socket);
+});
