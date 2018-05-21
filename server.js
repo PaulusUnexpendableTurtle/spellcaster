7d6d2f3e8b7ad0f0051e828e59adf047f4758717
Paul Sokolov Jr <sokolov.p64@gmail.com>
2018-05-21 22:01:00 +0300

    bug fixes & new cards sender
    Signed-off-by: Paul Sokolov Jr <sokolov.p64@gmail.com>

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const express = require('express'),
 const Parser = require('./lib/parser'),
       CardPool = require('./lib/game/card'),
       EventHandler = require('./lib/event'),
-      fileSystem = require('fs');
+      Papa = require('papaparse');
 
 const table_names =
 	[
 		{
 			name: 'cardData',
+			delimiter: ';',
 			header: true
 		},
 		{
@@ -26,12 +27,23 @@ const table_names =
 			name: 'sampleDecks'
 		}
 	];
-const parser = new Parser(fileSystem, require('papaparse'), table_names);
+const parser = new Parser(require('fs'), require('papaparse'), table_names);
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
+
+var transport, sourceJSON;
+if (process.argv[2]) {
+	transport = require('nodemailer').createTransport(sourceJSON = {
+		service: process.argv[2],
+		auth: {
+			user: process.argv[3],
+			pass: process.argv[4]
+		}
+	});
+}
 
 var eventHandler;
 io.sockets.on('connection', (socket) => {
@@ -45,14 +57,26 @@ io.sockets.on('connection', (socket) => {
 			cardPool: new CardPool(parser.get(table_names[0].name), ','),
 			subSplitter: '|',
 			cardPoolPoster: (card) => {
-				//TODO
+				if (!transport)
+					return;
+				transport.sendMail({
+					from: sourceJSON.auth.user,
+					to: sourceJSON.auth.user,
+					subject: 'Spellcaster: user posted new card',
+					text: Papa.unparse([card])
+				},
+				(error, info) => {
+					if (error)
+						console.log(error);
+					else
+						console.log(`Email sent: ${info.response}`);
+				});
 			},
 			Player: require('./lib/game/player'),
 			Game: require('./lib/game/game'),
 			gameOptions: parser.get(table_names[1].name),
 			gameEventTree: parser.get(table_names[2].name),
-			sampleDecks: parser.get(table_names[3].name),
-			fileSystem: fileSystem
+			sampleDecks: parser.get(table_names[3].name)
 		});
 	eventHandler.work(socket);
 });
